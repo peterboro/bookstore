9c939e531803f54f935e13f9256449adf6068d88
Peter Njuguna <Ptahbn@gmail.com>
2022-09-19 21:03:29 +0300

    Implementing removing and adding books

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import BookItem from '../components/BookItem';
 import AddBook from '../components/AddBook';
+import { useSelector } from 'react-redux';
 
 const Books = () => {
+  const books = useSelector((state) => state.books);
   return (
     <div>
-      <BookItem />
+      {
+        books.map(
+          (book) => (<BookItem key={book.id} id={book.id} title={book.title} author={book.author}/>),
+        )
+      }
       <AddBook />
     </div>
   );
