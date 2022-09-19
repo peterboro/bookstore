6dd5cde4f0d0aa6411a257042490b40b9379e035
Peter Njuguna <Ptahbn@gmail.com>
2022-09-19 21:05:59 +0300

    Define an action type for checking the status

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
+  const res = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
   return (
     <div className='categories'>
-      <button type="button">Check Status</button>
+      <span>{res}</span>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check Status</button>
     </div>
   );
 }
