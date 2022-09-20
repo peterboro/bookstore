0af94660303b49765c25830916bdfd0b63053da1
Peter Njuguna <Ptahbn@gmail.com>
2022-09-20 17:39:10 +0300

    refactor code to add book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -7,16 +7,18 @@ const AddBook = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
   const fetchTitle = (e) => setTitle(e.target.value);
   const fetchAuthor = (e) => setAuthor(e.target.value);
+  const fetchCategory = (e) => setCategory(e.target.value);
 
   const submitBook = (title, author) => {
-    dispatch(addBook({
-      id: uuidv4(),
-      title,
-      author,
-    }));
+    const newBook = {id: uuidv4(), title, author, category,};
+
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
   };
 
   return (
@@ -27,6 +29,13 @@ const AddBook = () => {
       </label>
       <label htmlFor="book-author">
       <input type="text" placeholder="Book Author" onChange={fetchAuthor} value={author} required />
+      </label>
+      <label htmlFor="book-categories">
+        <select className="categories" id="category" onChange={fetchCategory} value={category} required>
+          <option value="action">Action</option>
+          <option value="science">Science Fiction</option>
+          <option value="economy">Economy</option>
+        </select>
       </label>
       <button type="submit" onClick={() => submitBook(title, author)}>Add Book</button>
     </form>
